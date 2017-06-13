2d3133641975206d62316342ddbaa88aeb21a56f
BrunoHaspeslagh <bruno.haspeslagh@student.howest.be>
2017-06-13 12:43:59 +0200

    bugfix?

--- Stream.py
+++ Stream.py
@@ -131,7 +131,7 @@ def countvehicles(edge):
 sc = SparkContext(appName="Streaming")
 ssc = StreamingContext(sc, 10)
 lines = ssc.socketTextStream("172.23.80.245", 5580)
-edges = lines.flatMap(lambda xml: XmlParser.parsefullxml(xml)).map(lambda edge:"aantal wagens: "+ countvehicles(edge))
+edges = lines.flatMap(lambda xml: XmlParser.parsefullxml(xml)).map(lambda edge:countvehicles(edge))
 
 edges.pprint()
 
