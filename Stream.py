8ec22040f2afcce4322c5b652188db96c53e7d7b
BrunoHaspeslagh <bruno.haspeslagh@student.howest.be>
2017-06-14 11:17:31 +0200

    odpzkcsp

--- Stream.py
+++ Stream.py
@@ -142,6 +142,7 @@ def writeline(line):
 
 sc = SparkContext(appName="Streaming")
 ssc = StreamingContext(sc, 10)
+ssc.checkpoint("file:///tmp/spark")
 lines = ssc.socketTextStream("172.23.80.245", 5580).flatMap(lambda xml: XmlParser.parsefullxml(xml))
 edges = lines.map(lambda edge:"aantal wagens: "+ countvehicles(edge))
 
