7cce701b799988e5143cc76a64da040cae979bfc
BrunoHaspeslagh <bruno.haspeslagh@student.howest.be>
2017-06-14 09:21:54 +0200

    object test

--- Stream.py
+++ Stream.py
@@ -131,12 +131,12 @@ def countvehicles(edge):
 
 
 def writeline(line):
-    #v = line.id
-    driver = GraphDatabase.driver("bolt://pint-n2:7687", auth=basic_auth("neo4j", "Swh^bdl"), encrypted=False)
-    session = driver.session()
-    session.run("CREATE (n:Node {value: {v} })", {'v': v})
-    session.close()
-    return "doing something..."
+    v = line.id
+    #driver = GraphDatabase.driver("bolt://pint-n2:7687", auth=basic_auth("neo4j", "Swh^bdl"), encrypted=False)
+    #session = driver.session()
+    #session.run("CREATE (n:Node {value: {v} })", {'v': v})
+    #session.close()
+    return str(v)
 
 
 sc = SparkContext(appName="Streaming")
